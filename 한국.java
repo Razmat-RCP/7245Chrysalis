ffafde23706f854f36765392ede1bde9d8e82b88
rijulmehta <56002418+rijulmehta@users.noreply.github.com>
2019-10-25 17:37:31 -0400

    Update 한국.java
    Coded holonomic drive

--- 한국.java
+++ 한국.java
@@ -1,40 +1,57 @@
 package org.firstinspires.ftc.teamcode;
 
-//import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name= "한국", group = "Linear Opmode")
 //@Disabled
 public class 한국 extends LinearOpMode {
-    private DcMotor Thing1;
-    private DcMotor Thing2;
+    ElapsedTime runtime = new ElapsedTime();
+    DcMotor leftBack;
+    DcMotor leftFront;
+    DcMotor rightBack;
+    DcMotor rightFront;
 
     @Override
     public void runOpMode() {
-        //telemetry.addData("Abou: ", "Initialized"); //Abou tracker
+        leftBack = hardwareMap.get(DcMotor.class, "left_drive");
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
 
-        //boot dee scoopity woop
-        Thing1.setDirection(DcMotor.Direction.FORWARD);
-        Thing2.setDirection(DcMotor.Direction.REVERSE);
+        rightBack = hardwareMap.get(DcMotor.class, "right_drive");
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+
+        leftFront = hardwareMap.get(DcMotor.class, "left_drive");
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+
+        rightFront = hardwareMap.get(DcMotor.class, "right_drive");
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+
+        double speedAdjust = 7.0;
 
         waitForStart();
+        telemetry.addLine("Abou is being tracked...");
+        telemetry.addLine("...");
+        telemetry.addLine("Abou is located...");
+        runtime.reset();
 
+        while(opModeIsActive()){
+            if(gamepad1.dpad_left) {
+                speedAdjust -= 1;
+            }
+            if(gamepad1.dpad_right) {
+                speedAdjust += 1;
+            }
 
-        double leftPower;
-        double rightPower;
-
-        //tankdrive
-        leftPower = -gamepad1.left_stick_y ;
-        rightPower = -gamepad1.right_stick_y ;
-
-        //power things
-        Thing1.setPower(leftPower);
-        Thing2.setPower(rightPower);
-
-        //telemetry.addData("Abou: ", "Located"); //prints abous location to the phone
-
+            leftBack.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x)*(-speedAdjust/10));
+            rightBack.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x)*(-speedAdjust/10));
+            leftFront.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x)*(-speedAdjust/10));
+            rightFront.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x)*(-speedAdjust/10));
+            }
+        }
     }
-}
