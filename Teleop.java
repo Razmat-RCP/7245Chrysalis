6f4dc98390552394d1ea78dda2586c5f76d006ef
rijulmehta <56002418+rijulmehta@users.noreply.github.com>
2019-12-07 13:45:43 -0500

    Update Teleop.java
    Finished Teleop

--- Teleop.java
+++ Teleop.java
@@ -1,5 +1,7 @@
-//rleyvacortes22 is big brain 这是大脑时间
-//rmehta22
+/*
+rleyvacortes22 is big brain 这是大脑时间
+rmehta22
+*/
 
 package org.firstinspires.ftc.teamcode;
 
@@ -20,34 +22,71 @@ public class Teleop extends LinearOpMode {
     Servo gripper;
     DcMotor vertTrans;
     DcMotor horizTrans;
+    DcMotor leftRoller;
+    DcMotor rightRoller;
+    CRServo  leftServo;
+    CRServo rightServo;
 
     @Override
     public void runOpMode() {
-        leftBack = hardwareMap.get(DcMotor.class, "leftback");
-        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftBack.setDirection(DcMotor.Direction.FORWARD);
-
-        rightBack = hardwareMap.get(DcMotor.class, "rightback");
-        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightBack.setDirection(DcMotor.Direction.REVERSE);
-
-        leftFront = hardwareMap.get(DcMotor.class, "leftfront");
-        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftFront.setDirection(DcMotor.Direction.FORWARD);
-
-        rightFront = hardwareMap.get(DcMotor.class, "rightfront");
-        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightFront.setDirection(DcMotor.Direction.REVERSE);
-        gripper = hardwareMap.get(Servo.class, "gripper");
-        vertTrans = hardwareMap.get(DcMotor.class, "vertTrans");
-        horizTrans = hardwareMap.get(DcMotor.class, "horizTrans");
 
         double speedAdjust = 7.0;
+        double servoPos1 = 0.0;
 
-        telemetry.addLine("Abou is being tracked...");
-        telemetry.addLine("...");
-        telemetry.addLine("Abou is located...");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        gripper = hardwareMap.get(Servo.class, "gripper");
+
+        vertTrans = hardwareMap.get(DcMotor.class, "vertTrans");
+        vertTrans.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        vertTrans.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        vertTrans.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        horizTrans = hardwareMap.get(DcMotor.class, "horizTrans");
+        horizTrans.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        horizTrans.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        horizTrans.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftRoller = hardwareMap.get(DcMotor.class, "leftRoller");
+        leftRoller.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftRoller.setDirection(DcMotor.Direction.FORWARD);
+
+        rightRoller = hardwareMap.get(DcMotor.class, "rightRoller");
+        rightRoller.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRoller.setDirection(DcMotor.Direction.FORWARD);
+
+        leftServo = hardwareMap.get(CRServo.class, "leftServo");
+        rightServo = hardwareMap.get(CRServo.class, "rightServo");
+
+        gripper.setPosition(servoPos1);
+        leftServo.setPower(0.0);
+        rightServo.setPower(0.0);
+
         runtime.reset();
+
+
 
         waitForStart();
 
@@ -59,27 +98,109 @@ public class Teleop extends LinearOpMode {
                 speedAdjust += 1;
             }
 
-            if (gamepad2.right_bumper) {
-                gripper.setPosition(100);
+
+            //Dynamic Roller Code
+            if (gamepad1.left_trigger == 1) {
+                leftRoller.setPower(1.0);
+                rightRoller.setPower(-1.0);
+            }
+           else if (gamepad1.left_bumper) {
+                leftRoller.setPower(-1.0);
+                rightRoller.setPower(1.0);
+            }
+           else {
+               leftRoller.setPower(0.0);
+               rightRoller.setPower(0.0);
             }
 
-            if  (gamepad2.left_bumper) {
-                gripper.setPosition(-100);
+
+           //Stationary Roller Code
+            if (gamepad1.right_trigger == 1 ) {
+                leftServo.setPower(1.0);
+                rightServo.setPower(-1.0);
+            }
+            else if (gamepad1.right_bumper) {
+                leftServo.setPower(-1.0);
+                rightServo.setPower(1.0);
+            }
+            else {
+                leftServo.setPower(0.0);
+                rightServo.setPower(0.0);
             }
 
-            if (gamepad2.x) {
-                horizTrans.setPower(-1.0);
-            }
-            if (gamepad2.b) {
-                horizTrans.setPower(1.0);
-            }
 
-            vertTrans.setPower((gamepad2.right_trigger));
-            vertTrans.setPower(-(gamepad2.left_trigger));
-            leftBack.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x)*(-speedAdjust/10));
-            rightBack.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x)*(-speedAdjust/10));
+
+
+            //Gripper Position
+            if (gamepad2.left_bumper) {
+                servoPos1 += 0.05;
+
+                if (servoPos1 >= 0.5) {
+                    servoPos1 = 0.9;
+                }
+            }
+            else if (gamepad2.right_bumper) {
+                servoPos1 -= 0.05;
+
+                if (servoPos1 <= 0.5) {
+                    servoPos1 = 0.0;
+                }
+            }
+            gripper.setPosition(servoPos1);
+
+
+            //Horizontal Translator Code
+//            //make it go to left stick
+//            if (gamepad2.left_stick_y == 1.0) {
+//                horizTrans.setTargetPosition(0);
+//                horizTrans.setPower(-1.0);
+//            }
+//            else if (gamepad2.left_stick_y == -1.0) {
+//                horizTrans.setTargetPosition();
+//                horizTrans.setPower(1.0);
+//            }
+//            else {
+//                horizTrans.setPower(0.0);
+//            }
+
+
+
+            if (gamepad2.right_stick_y > 0) {
+                horizTrans.setTargetPosition(0);
+            }
+            else if (gamepad2.right_stick_y < 0) {
+                horizTrans.setTargetPosition(3700);
+            }
+            horizTrans.setPower(gamepad2.right_stick_y);
+
+
+
+
+            if (gamepad2.left_stick_y > 0) {
+                vertTrans.setTargetPosition(0);
+            }
+            else if (gamepad2.left_stick_y < 0) {
+                vertTrans.setTargetPosition(-4600);
+            }
+            vertTrans.setPower(-gamepad2.left_stick_y);
+
+
+
+
+            // Mecanum Driving Code
+
+            leftBack.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x)*(-speedAdjust/10));
+
+            rightBack.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x)*(-speedAdjust/10));
+
             leftFront.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x)*(-speedAdjust/10));
-            rightFront.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x)*(-speedAdjust/10));
+
+            rightFront.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x)*(-speedAdjust/10));
+
+
+            // Pace this loop so jaw action is reasonable speed.
+            sleep(20);
+            telemetry.update();
         }
     }
 }
